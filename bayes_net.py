9fbfa5cb48e1c1ce057c400f8ee618da5e7f467a
Tran-Nam <phuongnam9613@gmail.com>
2019-11-30 18:56:23 +0700

    add bayes node

--- bayes_net.py
+++ bayes_net.py
@@ -1,5 +1,7 @@
 import numpy as np 
 import pandas as pd 
+import itertools
+from pomegranate import *
 
 class Node():
     def __init__(self, name, predecessor):
@@ -7,13 +9,46 @@ class Node():
         self.predecessor = predecessor
     
     def set_prob(self, prob):
-        self.prob = prob
+        self.prob = DiscreteDistribution({'1': prob, '0': 1-prob})
         return self
 
     def create_cpt(self, cpt):
+        n_pres = len(self.predecessor)
+        mat = list(itertools.product('10', repeat=n_pres))
+        mat = [list(i) for i in mat]
+        for i in range(len(mat)):
+            mat[i].append(cpt[i])
+        self.cpt = mat
         return self 
 
     def calc_prob(self):
+        node = ConditionalProbabilityTable(
+            self.cpt,
+            self.predecessor
+        )
+
+        model = BayesianNetwork(self.name)
+        
+        node = State(node, name='node')
+        model.add_state(node)
+
+        for i in range(len(self.predecessor)):
+            par_state = State(self.predecessor[i], name='par_{}'.format(i))
+            model.add_state(par_state)
+            model.add_edge(par_state, node)
+        
+        model.bake()
+
+        mat = list(itertools.product('10', repeat=n_pres))
+        mat = [list(i) for i in mat]
+        
+        prob = 0
+        for i in range(len(mat)):
+            mat[i].append('1')
+            prob += model.probability[mat[i]]
+        
+        self.prob = DiscreteDistribution({'1': prob, '0': 1-prob})
+
         return self
 
 class Risk():
