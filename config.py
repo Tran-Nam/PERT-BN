e83cb48bf12ace7d6bc3bcdb35382ce90a22f2e7
Tran-Nam <phuongnam9613@gmail.com>
2019-12-02 17:48:23 +0700

    group data

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
-PROJECT_PATH = '1.csv'
-OUT_FIG = './fig'
-RELATION_RISK_PATH = 'risk_relate.txt'
-DISTRIBUTION_PATH = 'distribution.txt'
+import os
+
+PROJECT_PATH = 'project_data/0.csv'
+BASE_OUT_FIG = './fig_new'
+RELATION_RISK_PATH = 'risk_data_generate/risk_relate.txt'
+DISTRIBUTION_PATH = 'risk_data_generate/distribution_expert.txt'
+# RELATION_RISK_PATH = 'risk_data/risk_relate.txt'
+# DISTRIBUTION_PATH = 'risk_data/distribution.txt'
+project_name = PROJECT_PATH.split('/')[-1].split('.')[0]
+OUT_FIG = os.path.join(BASE_OUT_FIG, project_name)
