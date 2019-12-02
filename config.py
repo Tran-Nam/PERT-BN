3f5ba7b08514960cd04679ba76db0fe12e022782
Tran-Nam <phuongnam9613@gmail.com>
2019-12-02 21:44:57 +0700

    update config

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
-PROJECT_PATH = 'project_data/0.csv'
+PROJECT_PATH = 'project_data/2.csv'
 BASE_OUT_FIG = './fig_new'
 RELATION_RISK_PATH = 'risk_data_generate/risk_relate.txt'
-DISTRIBUTION_PATH = 'risk_data_generate/distribution_expert.txt'
+DISTRIBUTION_PATH = 'risk_data_generate/distribution_generate.txt'
 # RELATION_RISK_PATH = 'risk_data/risk_relate.txt'
 # DISTRIBUTION_PATH = 'risk_data/distribution.txt'
 project_name = PROJECT_PATH.split('/')[-1].split('.')[0]
